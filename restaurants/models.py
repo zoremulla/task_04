94614cc3610630d180f4a60197f2b6a8a2970567
MMulla <zore.mulla@gmail.com>
2019-07-10 19:59:11 +0300

    finished task4

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class restaurant(models.Model):
+    name=models.CharField(max_length=100)
+    description=models.TextField(max_length=200)
+    opening_time=models.TimeField(auto_now=False,auto_now_add=False)
+    closing_time=models.TimeField(auto_now=False,auto_now_add=False)
+
+    def __str__(self):
+        return "restaurant: " + self.name 
